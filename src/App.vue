c0a3f4b35a454df5c608a4201481f8c2ed8d77d0
Bakary <var.bsan@gmail.com>
2025-02-20 13:19:00 +0100

    implement vue devtools plugin; add matrix

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,42 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
-import { Button } from '@/components/ui/button'
-// Correct GraphQL query for graphqlzero.almansi.me
-const GET_POSTS = gql`
-  query GetPosts {
-    posts {
-      data {
-        id
-        title
-      }
-    }
-  }
-`;
 
-interface Post {
-  id: number;
-  title: string;
-}
+// Define the number of rows and columns
+const rows = 120;
+const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
 
-interface QueryResponse {
-  posts: {
-    data: Post[];
-  };
-}
-
-const client = new ApolloClient({
-  uri: 'https://graphqlzero.almansi.me/api',
-  cache: new InMemoryCache(),
-});
-
-const data = ref<QueryResponse>({ posts: { data: [] } });
-const loading = ref(false);
-const error = ref<string | null>(null);
-
-const fetchData = async () => {
-  loading.value = true;
-  error.value = null;
-
-  try {
-    const response = await client.query<QueryResponse>({
-      query: GET_POSTS,
+// Generate the cells for the matrix
+const cells = ref<{ col: string, row: number }[]>([]);
+for (let i = 1; i <= rows; i++) {
+  for (const col of cols) {
+    cells.value.push({
+      row: i,
+      col: col
     });
-    data.value = response.data;
-  } catch (err) {
-    error.value = (err as Error).message;
-  } finally {
-    loading.value = false;
   }
-};
+}
+
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 </script>
 
 <template>
-  <div class="p-4">
-    <h1 class="text-xl font-bold mb-2">GraphQL Data Fetch (Vue 3 + Apollo Client)</h1>
-    <Button @click="fetchData" class="cursor-pointer">
-      Fetch Data
-    </Button>
-
-    <div v-if="loading" class="text-gray-600">Loading...</div>
-    <div v-if="error" class="text-red-500">{{ error }}</div>
-
-    <ul v-if="data.posts?.data">
-      <li v-for="post in data.posts.data" :key="post.id" class="my-1">
-        {{ post.title }}
-      </li>
-    </ul>
+  <div class="bg-zinc-900 min-h-screen flex items-center justify-center p-4">
+    <div class="grid grid-cols-12 gap-0.5 w-full max-w-sm">
+      <div v-for="(cell, cellIndex) in cells" :key="cellIndex"
+        class="bg-transparent flex items-center justify-center text-transparent hover:text-white text-sm aspect-square border-1 border-purple-950">
+        <span class="absolute ">
+          {{ cell.col + cell.row }}
+        </span>
+        <div class="grid grid-cols-3 w-full gap-0.5">
+          <div 
+            v-for="square in squares" 
+            :key="square"
+            :id="cell.col + cell.row + '-' + square"
+            class="bg-zinc-600 flex items-center justify-center text-white aspect-square"
+          >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from 'node:url'
-
+import vueDevTools from 'vite-plugin-vue-devtools'
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
 
   resolve: {
     alias: {
