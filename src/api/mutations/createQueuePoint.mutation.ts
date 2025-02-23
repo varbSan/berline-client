a3b4253ba2eac3b7e2ff2c68fea3227b8ae8a0a5
Bakary <var.bsan@gmail.com>
2025-02-23 16:24:31 +0100

    chore

--- src/api/mutations/createQueuePoint.mutation.ts
+++ src/api/mutations/createQueuePoint.mutation.ts
@@ -1,5 +1,4 @@
 import { gql } from '@apollo/client/core';
-import { } from 'graphql.schema.json'
 export const CREATE_QUEUE_POINT_MUTATION = gql`
   mutation CreateQueuePoint($row: Int!) {
     createQueuePoint(row: $row) {
